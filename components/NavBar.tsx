cc38590f146946d535b35083e138624dc1cedccb
Anthony Murphy <awrmurphy@gmail.com>
2025-11-18 17:02:26 -0500

    initial commit with flat page and non-functioning navbar component

---
+++ components/NavBar.tsx
@@ -0,0 +1,11 @@
+export default function NavBar() {
+  return (
+    <div>
+      <ul>
+        <li>Home</li>
+        <li>Green Energy</li>
+        <li>Journal</li>
+      </ul>
+    </div>
+  );
+}
