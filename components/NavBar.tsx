ff3338ef4c1db31a86e7cbb582547a34a5d3b9b6
Anthony Murphy <awrmurphy@gmail.com>
2025-11-24 16:55:58 -0500

    mobile clarity styling update

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -5,19 +5,19 @@ export default function NavBar() {
     <div className="flex flex-row flex-auto justify-center">
       <Link
         href="/"
-        className="border-b-emerald-200 bg-emerald-700 p5 transition hover:bg-emerald-400 w-full text-black text-center "
+        className="border-b-emerald-200 bg-emerald-700 p5 transition hover:bg-emerald-400 focus:bg-emerald-400 w-full text-black text-center "
       >
         Home
       </Link>
       <Link
         href="/green"
-        className="border-b-emerald-200 bg-emerald-700 p5 transition hover:bg-emerald-400 w-full text-black text-center"
+        className="border-b-emerald-200 bg-emerald-700 p5 transition hover:bg-emerald-400 focus:bg-emerald-400 w-full text-black text-center"
       >
         Green Energy
       </Link>
       <Link
         href="/journal"
-        className="border-b-emerald-200 bg-emerald-700 p5 transition hover:bg-emerald-400 w-full text-black text-center"
+        className="border-b-emerald-200 bg-emerald-700 p5 transition hover:bg-emerald-400 focus:bg-emerald-400 w-full text-black text-center"
       >
         Journal
       </Link>
