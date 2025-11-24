1646b55bc16084d5aabca881c5fbf77e033fd86a
Anthony Murphy <awrmurphy@gmail.com>
2025-11-24 17:12:42 -0500

    added banner component with resizing for mobile and placeholder image

---
+++ components/Banner.tsx
@@ -0,0 +1,11 @@
+export default function Banner() {
+  return (
+    <div className="h-auto w-auto">
+      <img
+        src="/placeholderBanner.png"
+        alt="placeholder"
+        className="h-auto w-fit object-cover"
+      />
+    </div>
+  );
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import NavBar from "@/components/NavBar";
+import Banner from "@/components/Banner";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className="flex flex-col text-center">
+      <Banner />
       <NavBar />
       <Component {...pageProps} />
     </div>
