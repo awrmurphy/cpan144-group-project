4d8d2c7912877ecc558ac04a0027e6e2d3720ec7
Anthony Murphy <awrmurphy@gmail.com>
2025-12-12 13:20:03 -0500

    Updated api access

--- components/Weather.tsx
+++ components/Weather.tsx
@@ -13,7 +13,7 @@ export default function WeatherTile() {
   const [data, setData] = useState<WeatherData | null>(null);
   useEffect(() => {
     const fetchWeather = (lat: number, lon: number) => {
-      const url = `http://api.weatherstack.com/current?access_key=ceed63b7ededaff8cf715b5afed890f9&query=${lat},${lon}`;
+      const url = `http://api.weatherstack.com/current?access_key=cfade38566792df37a2b3600527dd007&query=${lat},${lon}`;
       const options = { method: "GET" };
       fetch(url, options)
         .then((res) => res.json())
@@ -35,7 +35,12 @@ export default function WeatherTile() {
     }
   }, []);
 
-  if (!data) return <div className="glass-card font-bold text-slate-700 p-2.5 w-24 h-24 fixed top-2 right-3 z-50 rounded-3xl shadow-lg hover:shadow-xl transition-shadow animate-none flex flex-col items-center justify-center bg-white/70">Loading Weather...</div>;
+  if (!data)
+    return (
+      <div className="glass-card font-bold text-slate-700 p-2.5 w-24 h-24 fixed top-2 right-3 z-50 rounded-3xl shadow-lg hover:shadow-xl transition-shadow animate-none flex flex-col items-center justify-center bg-white/70">
+        Loading Weather...
+      </div>
+    );
 
   return (
     <div className="glass-card p-2.5 w-24 h-24 fixed bottom-4 right-4 lg:top-2 lg:right-3 z-50 rounded-3xl shadow-lg hover:shadow-xl transition-shadow animate-none flex flex-col items-center justify-center bg-white/70">
@@ -43,7 +48,9 @@ export default function WeatherTile() {
         {data.location?.name}
       </h3>
       <div className="text-center space-y-0.5 flex flex-col items-center">
-        <p className="text-lg font-black text-emerald-700 leading-none">{data.current?.temperature}°C</p>
+        <p className="text-lg font-black text-emerald-700 leading-none">
+          {data.current?.temperature}°C
+        </p>
         <div className="flex items-center justify-center">
           <img
             src={data.current?.weather_icons[0]}
